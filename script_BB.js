feca3b042ec8fd7af35447b0af8662f92cff8345
タカギミサキ <68580568+TAKAGI-Misaki@users.noreply.github.com>
2020-08-29 16:03:56 +0900

    Add files via upload

--- script_BB.js
+++ script_BB.js
@@ -7,16 +7,16 @@ $(function(){
          
         if ($(window).scrollTop() > 850) {
         
-        $('.one').css('background-image','url(img/1.png)');
-        $('.two').css('background-image','url(img/3.png)'); 
-        $('.three').css('background-image','url(img/5.png)'); 
-        $('.four').css('background-image','url(img/6.png)'); 
+        $('.one').css('background-image','url(https://github.com/TAKAGI-Misaki/1313/blob/master/img/1.png?raw=true)');
+        $('.two').css('background-image','url(https://github.com/TAKAGI-Misaki/1313/blob/master/img/3.png?raw=true)'); 
+        $('.three').css('background-image','url(https://github.com/TAKAGI-Misaki/1313/blob/master/img/5.png?raw=true)'); 
+        $('.four').css('background-image','url(https://github.com/TAKAGI-Misaki/1313/blob/master/img/6.png?raw=true)'); 
         
         } else if ($(window).scrollTop() < 700) {
-        $('.one').css('background-image','url(img/1-long.png)');
-        $('.two').css('background-image','url(img/3-long.png)'); 
-        $('.three').css('background-image','url(img/5-long.png)'); 
-        $('.four').css('background-image','url(img/6-long.png)'); 
+        $('.one').css('background-image','url(https://github.com/TAKAGI-Misaki/1313/blob/master/img/1-long.png?raw=true)');
+        $('.two').css('background-image','url(https://github.com/TAKAGI-Misaki/1313/blob/master/img/3-long.png?raw=true)'); 
+        $('.three').css('background-image','url(https://github.com/TAKAGI-Misaki/1313/blob/master/img/5-long.png?raw=true)'); 
+        $('.four').css('background-image','url(https://github.com/TAKAGI-Misaki/1313/blob/master/img/6-long.png?raw=true)'); 
             
         } 
      });
